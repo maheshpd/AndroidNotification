75713801ba07cb3f792e925c964f4a1417bfa91a
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-06-03 07:48:41 +0530

    Handling Click

--- app/src/main/java/com/createsapp/androidnotificationdemo/NotificationHelper.java
+++ app/src/main/java/com/createsapp/androidnotificationdemo/NotificationHelper.java
@@ -1,6 +1,8 @@
 package com.createsapp.androidnotificationdemo;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -10,10 +12,19 @@ import static com.createsapp.androidnotificationdemo.MainActivity.CHANNEL_ID;
 public class NotificationHelper {
 
     public static void displayNotification(Context context, String title, String body) {
+
+        Intent intent = new Intent(context, ProfileActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,
+                100,
+                intent,
+                PendingIntent.FLAG_CANCEL_CURRENT);
+
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(title)
                 .setContentText(body)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
         NotificationManagerCompat mNotificationMgr = NotificationManagerCompat.from(context);
--- app/src/main/java/com/createsapp/androidnotificationdemo/ProfileActivity.java
+++ app/src/main/java/com/createsapp/androidnotificationdemo/ProfileActivity.java
@@ -26,6 +26,7 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+
         mAuth = FirebaseAuth.getInstance();
 
         FirebaseMessaging.getInstance().subscribeToTopic("updates");
